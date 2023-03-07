4765c3007830f611c33fc8038fa84ac0fb0907ba
kushvarma-solute <125448925+kushvarma-solute@users.noreply.github.com>
2023-03-07 11:54:25 +0530

    Update 7_finding_nth_largest_number.js
    Removing sort

--- 7_finding_nth_largest_number.js
+++ 7_finding_nth_largest_number.js
@@ -1,12 +1,16 @@
 //7. Find the nth largest element in a sorted array.
 let arr=[1,6,8,4,2,9,4];
-//Method 1:for unsorted Array;
-console.log("Method 1. Largest number in Array:", Math.max(...arr));
-//Method 2:for sorted Array;
-let n=2;
-arr.sort((a, b) => b - a); // sort the array in descending order
-if (n > arr.length) {
-  console.log("n is larger than array size");
-} else {
-    console.log("Method 2.",n,"th Largest number in Array:",arr[n-1]);
+let n=3;
+let temp=0;
+for(i =0;i<arr.length;i++){ 
+    if(arr[i]>arr[i+1]){
+      temp=arr[i];
+      arr[i]=arr[i+1];
+      arr[i+1]=temp;
+      i=-1;
+    }
+ 
 }
+arr=[...new Set(arr)]
+console.log(arr);
+console.log("The nth largest number is",arr[n-1]);
