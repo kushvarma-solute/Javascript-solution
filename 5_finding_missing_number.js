453d9a885793bcc05c8d2a0590bd6f47c5de28b3
kushvarma-solute <125448925+kushvarma-solute@users.noreply.github.com>
2023-03-07 10:41:17 +0530

    Update 5_finding_missing_number.js
    fixing logic

--- 5_finding_missing_number.js
+++ 5_finding_missing_number.js
@@ -5,8 +5,9 @@
 // Update the value of sum as sum = sum — array[i] + c and update c as c++.
 // Print the missing number as a sum.
 let sum=1;
-let c=2;
+
 let arr=[2,3,6,1,5,7,8]
+let c=arr[0];
 for(let i=0 ;i<arr.length;i++){
     sum=sum-arr[i]+c;
     c++;
